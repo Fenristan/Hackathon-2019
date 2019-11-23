6b0f7b7f4ba41c2d68770557b8080fb2d2377d90
wendyn70 <vaclav.nemec70@gmail.com>
2019-11-23 01:28:57 +0100

    BC Uploader
    Sends data containing colour information

---
+++ Uploader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using uPLibrary.Networking.M2Mqtt;
+
+class Motivation_Light_Uploader
+{
+    public static void Main(string[] args)
+    {
+        string id = "a5d91c0cc80d";
+        MqttClient client = new MqttClient("127.0.0.1");
+
+        client.Connect("id");
+
+        if (client.IsConnected)
+        {
+            Console.WriteLine("MQTT Connected Succesfully");
+            client.Publish($"node/{id}/led-strip/-/color/set", Encoding.ASCII.GetBytes(args[0]));
+        }
+
+
+        Console.ReadKey();
+
+        client.Disconnect();
+    }
+}
+
+
+
